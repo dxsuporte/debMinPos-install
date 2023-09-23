0bc8b12dbd15f875e95530687d1a4285817a3ffa
danilo <dxsuporteti@gmail.com>
2023-09-23 07:13:25 -0300

    debugs all

--- home.sh
+++ home.sh
@@ -1,3 +1,37 @@
 #!/bin/sh
 #Atualizar sistema
-apt update && apt -y upgrade
+nala update && nala upgrade -y
+#Otimizar bateria do notebook
+nala install -y tlp
+#Programas de Acessórios
+nala install -y galculator
+#Programas de Escritório
+nala install -y atril
+#Programas de Gráficos
+nala install -y inkscape gimp
+#Programas de Internet
+nala install -y firefox-esr qbittorrent thunar-dropbox-plugin
+#Programas de Multimídia
+nala install -y parole clementine
+#Chrome
+bash -c "echo 'deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main' >> /etc/apt/sources.list.d/google-chrome.list"
+wget -q -O - https://dl.google.com/linux/linux_signing_key.pub -O- | apt-key add -
+#Edge e VSCode
+bash -c "echo 'deb https://packages.microsoft.com/repos/edge stable main' >> /etc/apt/sources.list.d/microsoft-edge.list"
+bash -c "echo 'deb https://packages.microsoft.com/repos/vscode stable main' >> /etc/apt/sources.list.d/microsoft-vscode.list"
+wget -q -O - https://packages.microsoft.com/keys/microsoft.asc -O- | apt-key add -
+#OnlyOffice
+bash -c "echo deb 'https://download.onlyoffice.com/repo/debian squeeze main' >> /etc/apt/sources.list.d/onlyoffice.list"
+wget -q -O - http://download.onlyoffice.com/repo/onlyoffice.key -O- | apt-key add -
+#AnyDesk
+bash -c "echo deb 'http://deb.anydesk.com/ all main' >> /etc/apt/sources.list.d/anydesk-stable.list"
+wget -q -O - https://keys.anydesk.com/repos/DEB-GPG-KEY -O- | apt-key add -
+#TeamViewer
+bash -c "echo 'deb http://linux.teamviewer.com/deb stable main' >> /etc/apt/sources.list.d/teamviewer.list"
+wget -q -O - https://download.teamviewer.com/download/linux/signature/TeamViewer2017.asc -O- | apt-key add -
+nala update
+nala install -y google-chrome-stable
+nala install -y microsoft-edge-stable
+nala install -y code
+nala install -y anydesk
+nala install -y teamview
--- root.sh
+++ root.sh
@@ -7,110 +7,77 @@ sed -i 's/main non-free-firmware/main non-free-firmware contrib non-free/g' /etc
 #Mudar lingual do Sistema
 update-locale LANG=pt_BR.UTF-8
 locale-gen --purge pt_BR.UTF-8
-#Instala interface para APT
+#Interface para APT
 apt install -y nala
 #Atualizar sistema
 nala update && nala upgrade -y
-#
-nala install -y firmware-linux
-nala install -y intel-microcode
+#Drives
+#nala install -y intel-microcode
 #nala install -y amd64-microcode
+nala install -y firmware-linux
 nala install -y linux-headers-$(uname -r)
-#Ferramenta
-nala install -y bash-completion curl wget software-properties-common apt-transport-https
-#Instalar Interface grafica XFCE4
+nala install -y intel-gpu-tools
+#Interface grafica XFCE4
 nala install -y xfce4-terminal
 nala install -y xorg
 nala install -y xfce4
 nala install -y xfce4-goodies
 nala install -y xfce4-*
+#Interface Carregamento do X
 nala install -y lightdm lightdm-gtk-greeter-settings
-nala install -y python3-gi python3-psutil
-nala install -y menulibre mugshot
-#Instalar Complementos do sistema
-nala install -y intel-gpu-tools xdg-user-dirs-gtk optipng
-#Instalar Fonts
-nala install -y fonts-noto fonts-noto-core fonts-firacode fonts-powerline
-#Otimizar bateria do notebook
-nala install -y tlp
-#Instalar Theme
 nala install -y plymouth plymouth-themes
-nala install -y orchis-gtk-theme greybird-gtk-theme
-nala install -y elementary-xfce-icon-theme breeze-cursor-theme
-#Instalar gerenciador de rede, usuários, impressora e software para X.
-nala install -y network-manager
-nala install -y network-manager-gnome
-nala install -y gnome-system-tools
-nala install -y system-config-printer
-nala install -y software-properties-gtk
-#Instalar Ferramentas do Sistema
-nala install -y synaptic gparted gufw
-nala install -y neofetch blueman
-nala install -y parole clementine
+#Ferramenta do Sistema
+nala install -y gnome-system-tools gufw gparted xdg-user-dirs-gtk
+nala install -y menulibre mugshot python3-gi python3-psutil
+#Gerenciador de software.
+nala install -y software-properties-gtk synaptic
+#Gerenciador de rede.
+nala install -y network-manager network-manager-gnome blueman
 #Instalar Compartilhamento de rede
 nala install -y samba smbclient wsdd wsdd2
-nala install -y gvfs-backends gvfs-fuse
+nala install -y gvfs-backends gvfs-fuse gigolo
 #Instalar Software Impressora HP
+nala install -y system-config-printer
 nala install -y hplip printer-driver-all
-#Instalar outro programas
-nala install -y firefox-esr thunar-dropbox-plugin
-nala install -y inkscape gimp qbittorrent
-nala install -y galculator atril gigolo
+#Ultilitario de Terminal
+nala install -y neofetch bash-completion
+nala install -y curl wget software-properties-common apt-transport-https
+#Instalar Fonts
+nala install -y fonts-noto fonts-noto-core fonts-firacode fonts-powerline
+#Instalar Theme
+nala install -y orchis-gtk-theme greybird-gtk-theme
+nala install -y elementary-xfce-icon-theme breeze-cursor-theme
 #Codecs para descompaquitar arquivos
 nala install -y zip p7zip* unrar* rar arc arj cabextract lhasa unace xz-utils
 #Codecs de audio e video
-#nala install -y ffmpeg faad lame sox twolame vorbis-tools
-#nala install -y libavcodec-extra* libavdevice60 libgstreamer1.0-0
-#nala install -y gstreamer1.0-fdkaac gstreamer1.0-libav gstreamer1.0-vaapi gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly
-#Programas de terceiros
-#Instalar e configurar tema no bash do root
-bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)" --unattended || echo 'OK'
-sed -i 's/OSH_THEME="font"/OSH_THEME="rjorgenson"/g' /root/.bashrc
-#Instalar e configurar tema no bash do 1º usuario
-runuser -l $(id 1000 -u -n) -c 'bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)" --unattended' || echo 'OK'
-sed -i 's/OSH_THEME="font"/OSH_THEME="powerline-light"/g' /home/$(id 1000 -u -n)/.bashrc
-#Chrome
-bash -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
-wget -q -O - https://dl.google.com/linux/linux_signing_key.pub -O- | apt-key add -
-#Edge e VSCode
-bash -c "echo 'deb https://packages.microsoft.com/repos/edge stable main' >> /etc/apt/sources.list.d/microsoft-edge.list"
-bash -c "echo 'deb https://packages.microsoft.com/repos/vscode stable main' >> /etc/apt/sources.list.d/microsoft-vscode.list"
-wget -q -O - https://packages.microsoft.com/keys/microsoft.asc -O- | apt-key add -
-#AnyDesk
-echo "deb http://deb.anydesk.com/ all main" >/etc/apt/sources.list.d/anydesk-stable.list
-wget -q -O - https://keys.anydesk.com/repos/DEB-GPG-KEY -O- | apt-key add -
-#TeamViewer
-bash -c "echo 'deb http://linux.teamviewer.com/deb stable main' >> /etc/apt/sources.list.d/teamviewer.list"
-wget -q -O - https://download.teamviewer.com/download/linux/signature/TeamViewer2017.asc -O- | apt-key add -
-nala update
-nala install -y google-chrome-stable
-nala install -y microsoft-edge-stable
-nala install -y code
-nala install -y anydesk
-nala install teamviewer --assume-yes
-#Substituir o arquivo inface para network-manager ter controle da rede no X
+nala install -y ffmpeg faad lame sox twolame vorbis-tools libavcodec-extra* gstreamer1.0-fdkaac
+#-----------------------------Configurações do Sistema-----------------------------------------#
+#Gerenciar Rede modo grafico
 cp -f config/interfaces /etc/network/interfaces
-#Configuração da tela de login
-cp -f config/lightdm-gtk-greeter.conf /etc/lightdm
-#Menu e painel de configuraçães do xfce
+#Menu e configuraçães do xfce
 cp -f menu/* /etc/xdg/menus
 cp -f xfce/* /etc/xdg/xfce4/xfconf/xfce-perchannel-xml
+#Tema da tela de login
+cp -f config/lightdm-gtk-greeter.conf /etc/lightdm
 #Icones
 cp -f dx-logo.svg /usr/share/icons/hicolor/scalable/apps
 #Habilitar Mostra usuario no login
 sed -i 's/#greeter-hide-users=false/greeter-hide-users=false/g' /etc/lightdm/lightdm.conf
+#Backgrounds
+mv -n backgrounds/* /usr/share/backgrounds
+#Modelo Thema Panel XFCE
+mv -n theme/W7.tar.bz2 /usr/share/xfce4-panel-profiles/layouts/
 #Habilitar theme de carregamento do sistemax
 sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT="quiet"/GRUB_CMDLINE_LINUX_DEFAULT="quiet splash"/g' /etc/default/grub
 sed -i 's/#GRUB_GFXMODE=640x480/GRUB_GFXMODE=1024x768/g' /etc/default/grub
-plymouth-set-default-theme -R bgrt
-#Backgrounds
-mv -n backgrounds/* /usr/share/backgrounds
-#Theme Panel XFCE
-mv -n theme/W7.tar.bz2 /usr/share/xfce4-panel-profiles/layouts/
-update-grub2
-#Remove interface para APT
-nala remove -y nala --remove-essential
+plymouth-set-default-theme -R bgrt && update-grub2
+#Instalar e configurar tema no bash do root
+bash -c "$(wget https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh -O -)" --unattended || echo 'OK'
+sed -i 's/OSH_THEME="font"/OSH_THEME="rjorgenson"/g' /root/.bashrc
+#Instalar e configurar tema no bash do 1º usuario
+runuser -l $(id 1000 -u -n) -c 'bash -c "$(wget https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh -O -)" --unattended' || echo 'OK'
+sed -i 's/OSH_THEME="font"/OSH_THEME="powerline-light"/g' /home/$(id 1000 -u -n)/.bashrc
 #Limpeza no apt
-apt autoremove -y && apt autoclean && apt clean
+nala autoremove -y && apt autoclean && nala clean
 #Reinicia o sistema
 reboot
