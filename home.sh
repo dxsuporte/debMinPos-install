51561f84e4a672f6f20bc30a14a8434c16901d9a
danilo <dxsuporteti@gmail.com>
2023-09-23 21:48:09 -0300

    Antares SQL e Mega

--- home.sh
+++ home.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 set -e
+#Versão do Debian
+[ $(lsb_release -rs) = "n/a" ] && { RELEASE="testing" || RELEASE=$(lsb_release -rs); }
 #Atualizar sistema
 add-apt-repository -y "deb http://deb.debian.org/debian/ oldstable main contrib non-free"
 nala update && nala upgrade -y
@@ -17,6 +19,13 @@ nala install -y chromium
 nala install -y chromium-l10n
 #Programas de Multimídia
 nala install -y clementine
+#Mega
+wget -c https://mega.nz/linux/repo/Debian_"$RELEASE"/amd64/megasync-Debian_"$RELEASE"_amd64.deb
+wget -c https://mega.nz/linux/repo/Debian_"$RELEASE"/amd64/thunar-megasync-Debian_"$RELEASE"_amd64.deb
+nala install -y ./megasync*.deb
+nala install -y ./thunar*.deb
+rm -R ./megasync*.deb
+rm -R ./thunar*.deb
 #OnlyOffice
 wget -c https://download.onlyoffice.com/install/desktop/editors/linux/onlyoffice-desktopeditors_amd64.deb
 nala install -y ./onlyoffice*.deb
@@ -30,6 +39,11 @@ add-apt-repository -y "deb http://deb.anydesk.com/ all main"
 wget -q -O - https://keys.anydesk.com/repos/DEB-GPG-KEY -O- | apt-key add -
 nala update
 nala install -y anydesk
+#Antares SQL
+add-apt-repository -y "deb https://antares-sql.github.io/antares-ppa ./"
+wget -q -O - https://antares-sql.github.io/antares-ppa/key.gpg -O- | apt-key add -
+nala update
+nala install -y antares
 #Edge e VSCode
 add-apt-repository -y "deb https://packages.microsoft.com/repos/edge stable main"
 add-apt-repository -y "deb https://packages.microsoft.com/repos/vscode stable main"
