37ac61fed9918256bdb02a64501f247f4d58c9e9
danilo <dxsuporteti@gmail.com>
2023-12-15 13:59:45 -0300

    config utilities

--- main.sh
+++ main.sh
@@ -18,6 +18,8 @@ apt install -y firmware-amd-graphics
 apt install -y firmware-iwlwifi
 apt install -y firmware-atheros
 apt install -y firmware-realtek
+apt install -y firmware-b43legacy-installer
+apt install -y firmware-b43-installer
 apt install -y intel-microcode intel-gpu-tools
 apt install -y amd64-microcode amdgcn-tools
 #INTEL e AMD
@@ -41,7 +43,7 @@ apt install -y mousepad xarchiver
 apt install -y xdg-user-dirs xdg-user-dirs-gtk
 apt install -y elementary-xfce-icon-theme
 #Fonts Padrão
-
+#apt install -y fonts-noto* fonts-powerline fonts-firacode
 #Software Padrão
 apt install -y firefox-esr atril gnome-calculator
 #Gerenciador Login
@@ -56,7 +58,7 @@ apt install -y bash-completion command-not-found
 apt install -y zip p7zip* unrar* unzip rar arc arj cabextract lhasa unace* xz-utils sharutils uudeview mpack
 #Codecs de audio e video
 apt install -y ffmpeg mencoder faad lame sox twolame vorbis-tools libavcodec-extra* gstreamer1.0-fdkaac gstreamer1.0-plugins-ugly
-
+####Config####
 #Habilitar Theme Grub
 sed -i 's/#GRUB_GFXMODE=640x480/GRUB_GFXMODE=1024x768/g' /etc/default/grub
 sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT="quiet"/GRUB_CMDLINE_LINUX_DEFAULT="quiet splash modprobe.blacklist=pcspkr"/g' /etc/default/grub
@@ -75,13 +77,16 @@ cp -f config/interfaces /etc/network/interfaces
 #Aplicar Thema do Boot e Atualizar Boot
 plymouth-set-default-theme -R bgrt
 update-grub2
-#Limpeza no apt
-apt autoremove -y
-apt autoclean
-apt clean
-
-source utilities.sh
-
-#Reinicia o sistema
-read -r -p "Instalação concluida! Seu pc precisa ser reiniciad! [Enter] " REBOOT
-reboot
+#Reiniciar ou Software Extra
+read -r -p "Deseta instalar os programas complementares? [y|n] " SOFTWARE
+if [ "$SOFTWARE" = "n" ]; then
+    #Limpeza no apt
+    apt autoremove -y
+    apt autoclean
+    apt clean
+    #Reinicia o sistema
+    read -r -p "Instalação concluida! O sistema será reinicializado! [Enter] " REBOOT
+    reboot
+else
+    sh utilities.sh
+fi
--- utilities.sh
+++ utilities.sh
@@ -1,22 +1,105 @@
 #!/bin/sh
 #Interromper o script se algum comando falhar.
 set -e
-#Gerenciador de bluetooth.
-apt install -y bluez blueman pulseaudio-module-bluetooth
-#Ferramenta do Sistema
-apt install -y synaptic
-apt install -y gparted
-apt install -y gnome-disk-utility
-apt install -y baobad
-apt install -y gufw
-apt install -y mintstick
-#Instalar Fonts
-#apt install -y fonts-noto* fonts-powerline fonts-firacode
-#Programas de Básicos
-apt install -y qbittorrent
-apt install -y gnome-sound-recorder
-#Programas de Shell
-apt install -y neofetch
-apt install -y arping
-#Codecs CD/DVD
-apt install -y libdvd-pkg
+######################################################################
+read -r -p "Instalar Utilitario Terminal? [y|n] " TERMINAL
+read -r -p "Instalar gufw? [y|n] " FIREWALL
+read -r -p "Instalar synaptic? [y|n] " SYNAPTIC
+read -r -p "Instalar gparted? [y|n] " GPARTED
+read -r -p "Instalar baobab? [y|n] " BAOBAB
+read -r -p "Instalar Gnome Disk? [y|n] " GNOMEDISK
+read -r -p "Instalar mintstick? [y|n] " MINTSTICK
+read -r -p "Instalar Torrent? [y|n] " TORRENT
+read -r -p "Instalar bluetooth? [y|n] " BLUETOOTH
+read -r -p "Instalar gnome sound recorder? [y|n] " GNOMERECORDER
+read -r -p "Instalar CD/DVD? [y|n] " DVD
+read -r -p "Gostaria de otimizar a bateria do notebook? [y|n] " TLP
+read -r -p "Instalar o Software para WebCam? [y|n] " CHEESE
+read -r -p "Instalar o Software e drives para Impressoras? [y|n] " IMP
+read -r -p "Instalar a loja de Software? [y|n] " SOFTWAREDEB
+######################################################################
+#Gerenciador de TERMINAL.
+if [ "$TERMINAL" = "y" ]; then
+    apt install -y neofetch arping
+    #Bash User Root
+    bash -c "$(wget https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh -O -)" --unattended || echo 'OK'
+    sed -i 's/OSH_THEME="font"/OSH_THEME="zork"/g' /root/.bashrc
+    #Bash User First Home
+    runuser -l $(id 1000 -u -n) -c 'bash -c "$(wget https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh -O -)" --unattended' || echo 'OK'
+    sed -i 's/OSH_THEME="font"/OSH_THEME="mairan"/g' /home/$(id 1000 -u -n)/.bashrc
+fi
+#Gerenciador de FIREWALL.
+if [ "$FIREWALL" = "y" ]; then
+    apt install -y gufw
+fi
+#Gerenciador de SYNAPTIC.
+if [ "$SYNAPTIC" = "y" ]; then
+    apt install -y synaptic
+fi
+#Gerenciador de GPARTED.
+if [ "$GPARTED" = "y" ]; then
+    apt install -y gparted
+fi
+#Gerenciador de BAOBAB.
+if [ "$BAOBAB" = "y" ]; then
+    apt install -y baobab
+fi
+#Gerenciador de GNOMEDISK.
+if [ "$GNOMEDISK" = "y" ]; then
+    apt install -y gnome-disk-utility
+fi
+#Gerenciador de MINTSTICK.
+if [ "$MINTSTICK" = "y" ]; then
+    apt install -y mintstick
+fi
+#Gerenciador de TORRENT.
+if [ "$TORRENT" = "y" ]; then
+    apt install -y qbittorrent
+fi
+#Gerenciador de BLUETOOTH.
+if [ "$BLUETOOTH" = "y" ]; then
+    apt install -y bluez blueman pulseaudio-module-bluetooth
+fi
+#Gerenciador de GNOMERECORDER.
+if [ "$GNOMERECORDER" = "y" ]; then
+    apt install -y gnome-sound-recorder
+fi
+#Gerenciador de DVD.
+if [ "$DVD" = "y" ]; then
+    apt install -y libdvd-pkg
+fi
+#Otimizar bateria do notebook
+if [ "$TLP" = "y" ]; then
+    apt install -y laptop-mode-tools
+    apt install -y tlp
+fi
+#Cheese webcam
+if [ "$CHEESE" = "y" ]; then
+    apt install -y cheese
+fi
+#Sofware e drives Impressoras
+if [ "$IMP" = "y" ]; then
+    apt install -y system-config-printer
+    apt install -y printer-driver-all
+    apt install -y printer-driver-escpr
+    apt install -y printer-driver-gutenprint
+    apt install -y cups
+    apt install -y hplip
+    apt install -y hp-ppd
+    apt install -y openprinting-ppds
+    apt install -y simple-scan
+fi
+#Software DEB
+if [ "$SOFTWAREDEB" = "y" ]; then
+    apt install -y gnome-software
+else
+    apt install -y deepin-deb-installer
+    apt install -y gnome-packagekit
+fi
+#Limpeza no apt
+apt autoremove -y
+apt autoclean
+apt clean
+#Reinicia o sistema
+read -r -p "Instalação concluida! Seu pc precisa ser reiniciad! [Enter] " REBOOT
+reboot
