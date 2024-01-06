bfaf9c092a04b76866e30077623a1b81189ca05e
danilo <dxsuporteti@gmail.com>
2024-01-06 16:04:34 -0300

    samba up

--- samba/main.sh
+++ samba/main.sh
@@ -1,56 +1,32 @@
 #!/bin/sh
-#Stop the script if any command fails. || #Interromper o script se algum comando falhar.
+#Interromper o script se algum comando falhar.
 set -e
-#APT or NALA
-if [ "$(dpkg -l nala 2>&- | grep -c ^ii)" = 1 ]; then
-    PRG="nala"
-else
-    PRG="apt"
-fi
-#Atualizar sistema e Instalar Compartilhamento de rede
-$PRG update
-$PRG install -y samba smbclient cifs-utils wsdd wsdd2
-#-----------------------------Configurações do Sistema-----------------------------------------#
+#Include
+. "$(pwd)/../myInclude.sh"
+#Atualizar sistema e Instalar Samba
+$myPRG update
+$myPRG install -y samba smbclient cifs-utils wsdd wsdd2
 #Samba config
 sed -i 's/;   bind interfaces only = yes/bind interfaces only = yes/g' /etc/samba/smb.conf
 if [ $(grep -c '/home/samba/public' /etc/samba/smb.conf) = 0 ]; then
-    echo "
-    [public]
-    path = /home/samba/public
-    writable = yes
-    guest ok = yes
-    guest only = yes
-    force create mode = 777
-    force directory mode = 777
-    " | tee -a /etc/samba/smb.conf
+    cat public | tee -a /etc/samba/smb.conf
 fi
 if [ $(grep -c '/home/samba/private' /etc/samba/smb.conf) = 0 ]; then
-    echo "
-    [private]
-    security = user
-    path = /home/samba/private
-    writable = yes
-    guest ok = no
-    valid users = @sambashare
-    force group = sambashare
-    force create mode = 770
-    force directory mode = 770
-    inherit permissions = yes
-    " | tee -a /etc/samba/smb.conf
+    cat private | tee -a /etc/samba/smb.conf
 fi
-mkdir -m 777 -p /home/samba/public
-mkdir -m 770 -p /home/samba/private
+mkdir -m 777 -p /home/samba/public /home/samba/private
 chgrp sambashare /home/samba/private
 usermod -aG sambashare $(id 1000 -u -n)
 smbpasswd -a $(id 1000 -u -n)
-echo "[Desktop Entry]" | tee /etc/xdg/autostart/samba-public.desktop
-echo 'Exec=sh -c "[ ! -d "$HOME/share" ] && { ln -s /home/samba $HOME/Compartilhado; }"' | tee -a /etc/xdg/autostart/samba-public.desktop
-#Limpeza apt
-$PRG autoremove -y && apt autoclean && apt clean
+#AutoStart
+cp -f -r samba-public.desktop /etc/xdg/autostart/
+#----------End----------#
+#Atualizar Grub, Limpeza apt
+$myPRG autoremove -y && apt autoclean && apt clean
 #Reinicia o sistema
 read -r -p "Instalação concluida! Seu pc precisa ser reiniciad! [Enter] " REBOOT
 if [ "$REBOOT" = "y" ]; then
-    reboot
+    rm -f -r /tmp/* && reboot
 else
-    reboot
+    rm -f -r /tmp/* && reboot
 fi
