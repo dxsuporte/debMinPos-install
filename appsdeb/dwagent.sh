2f8a746e3784c87153c738dc4fa87ed814c5d3a9
danilo <dxsuporteti@gmail.com>
2024-01-06 08:24:35 -0300

    rm prg dwagent

--- appsdeb/dwagent.sh
+++ appsdeb/dwagent.sh
@@ -1,12 +1,6 @@
 #!/bin/sh
 #Interromper o script se algum comando falhar.
 set -e
-#APT or NALA
-if [ "$(dpkg -l nala 2>&- | grep -c ^ii)" = 1 ]; then
-    PRG="nala"
-else
-    PRG="apt"
-fi
 #----------Start----------#
 wget -cO - https://node213098.dwservice.net/getAgentFile.dw?name=dwagent.sh -O /tmp/dwagent.sh
 chmod +x /tmp/dwagent.sh
