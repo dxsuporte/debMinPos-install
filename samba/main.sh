8999be273118eb7ebff95337749d8e264283dcc1
danilo <dxsuporteti@gmail.com>
2024-01-06 18:17:27 -0300

    link debug

--- samba/main.sh
+++ samba/main.sh
@@ -6,19 +6,19 @@ set -e
 #Atualizar sistema e Instalar Samba
 $myPRG update && $myPRG install -y samba smbclient cifs-utils wsdd wsdd2
 #Criar Pasta public e private
-mkdir -m 777 -p /home/Samba/Public /home/Samba/Private
+mkdir -m 777 -p /home/Compartilhado/Public /home/Compartilhado/Private
 #Samba config
 sed -i 's/;   bind interfaces only = yes/bind interfaces only = yes/g' /etc/samba/smb.conf
 #Criar configuração da Pasta public e private no samba
-grep -c '/home/Samba/Public' /etc/samba/smb.conf || cat public | tee -a /etc/samba/smb.conf
-grep -c '/home/Samba/Private' /etc/samba/smb.conf || cat private | tee -a /etc/samba/smb.conf
+grep -c '/home/Compartilhado/Public' /etc/samba/smb.conf || cat public | tee -a /etc/samba/smb.conf
+grep -c '/home/Compartilhado/Private' /etc/samba/smb.conf || cat private | tee -a /etc/samba/smb.conf
 #Adicionar Usuário, Senha do Usuário e Pasta private ao SambaShare
 usermod -aG sambashare $(id 1000 -u -n)
 smbpasswd -a $(id 1000 -u -n)
-chgrp sambashare /home/Samba/Private
+chgrp sambashare /home/Compartilhado/Private
 #Criar Link e Atlaho gernciador de aquivo
-ln -sf /home/Samba/ /home/$(id 1000 -u -n)/Samba
-echo "file:///home/$(id 1000 -u -n)/Samba" | tee -a /home/$(id 1000 -u -n)/.config/gtk-3.0/bookmarks
+ln -sf /home/Compartilhado/ /home/$(id 1000 -u -n)/Compartilhado
+echo "file:///home/$(id 1000 -u -n)/Compartilhado" | tee -a /home/$(id 1000 -u -n)/.config/gtk-3.0/bookmarks
 #----------End----------#
 #Atualizar Grub, Limpeza apt
 $myPRG autoremove -y && apt autoclean && apt clean
