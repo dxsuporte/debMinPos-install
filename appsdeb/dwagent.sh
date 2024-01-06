5c380bad5e73858103aab11b78ac9bb1b0380da7
danilo <dxsuporteti@gmail.com>
2024-01-06 06:44:55 -0300

    dwagent

--- appsdeb/dwagent.sh
+++ appsdeb/dwagent.sh
@@ -4,6 +4,6 @@ set -e
 #APT or NALA
 PRG="$1"
 #----------Start----------#
-wget -cO - https://node213098.dwservice.net/getAgentFile.dw?name=dwagent.sh >dwagent.sh
+wget -cO - https://node213098.dwservice.net/getAgentFile.dw?name=dwagent.sh -O ./temp/dwagent.sh >dwagent.sh
 chmod +x dwagent.sh && sh dwagent.sh
 rm -f -r ./dwagent.sh
