ddc17228d851c56b558da40f5224e3ab12b7d625
danilo <dxsuporteti@gmail.com>
2024-01-07 13:57:33 -0300

    xamp new

--- xampp/main.sh
+++ xampp/main.sh
@@ -7,22 +7,23 @@ if [ -e ./xampp-linux*.run ]; then
     chmod +x xampp-linux*.run
     #Executa o instalador Xampp.
     ./xampp-linux*.run
-    #Copiá o script de configuração da permissão pkexec.
-    cp -f config/org.apachefriends.xampp-control-panel.policy /usr/share/polkit-1/actions/
-    #Copiá o script de execução pelo terminal.
-    cp -f config/xampp-control-panel /usr/bin/
-    #Copiá a script do ícone para o menu.
-    cp -f config/xampp-control-panel.desktop /usr/share/applications/
-    #Copiá a imagem do ícone para o menu.
-    cp -f config/xampp.svg /usr/share/icons/
-    #Copiá o script de configuração do systemd.
-    cp -f config/xampp.service /etc/systemd/system/
-    #Habilita o script no systemd para iniciar no boot.
-    systemctl enable xampp.service
-    #Mensagem para o usuário.
-    echo "Foi instalado com sucesso"
-
 else
-    #Mensagem para o usuário.
-    echo "Erro ao instalar o XAMPP! O XAMPP deve ser baixado e colocado dentro da pasta principal do instalador."
+    RELEASE=$(curl -s https://sourceforge.net/projects/xampp/files/XAMPP%20Linux/ | grep -m1 "net.sf.files" | cut -d '"' -f 2)
+    wget -c https://sinalbr.dl.sourceforge.net/project/xampp/XAMPP%20Linux/"$RELEASE"/xampp-linux-x64-"$RELEASE"-0-installer.run -O /tmp/xampp.run
+    chmod +x /tmp/xampp.run
+    /tmp/xampp.run
 fi
+#Copiá o script de configuração da permissão pkexec.
+cp -f config/org.apachefriends.xampp-control-panel.policy /usr/share/polkit-1/actions/
+#Copiá o script de execução pelo terminal.
+cp -f config/xampp-control-panel /usr/bin/
+#Copiá a script do ícone para o menu.
+cp -f config/xampp-control-panel.desktop /usr/share/applications/
+#Copiá a imagem do ícone para o menu.
+cp -f config/xampp.svg /usr/share/icons/
+#Copiá o script de configuração do systemd.
+cp -f config/xampp.service /etc/systemd/system/
+#Habilita o script no systemd para iniciar no boot.
+systemctl enable xampp.service
+#Mensagem para o usuário.
+echo "Foi instalado com sucesso"
