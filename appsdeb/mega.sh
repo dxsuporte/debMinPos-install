2b8f11b8f57f63cc4e4d75210fee6455c918501a
danilo <dxsuporteti@gmail.com>
2024-06-02 15:17:31 -0300

    up

--- appsdeb/mega.sh
+++ appsdeb/mega.sh
@@ -10,7 +10,9 @@ fi
 #----------Start----------#
 curl -fsSL https://mega.nz/linux/repo/Debian_"$RELEASE"/amd64/megasync-Debian_"$RELEASE"_amd64.deb -o /tmp/megasync-Debian_amd64.deb
 apt install -y /tmp/megasync-Debian_amd64.deb
+curl -fsSL https://mega.nz/linux/repo/Debian_"$RELEASE"/amd64/nautilus-megasync-Debian_"$RELEASE"_amd64.deb -o /tmp/nautilus-megasync-Debian_amd64.deb
+curl -fsSL https://mega.nz/linux/repo/Debian_"$RELEASE"/amd64/nemo-megasync-Debian_"$RELEASE"_amd64.deb -o /tmp/nemo-megasync-Debian_amd64.deb
 curl -fsSL https://mega.nz/linux/repo/Debian_"$RELEASE"/amd64/thunar-megasync-Debian_"$RELEASE"_amd64.deb -o /tmp/thunar-megasync-Debian_amd64.deb
+apt install -y /tmp/nautilus-megasync-Debian_amd64.deb
+apt install -y /tmp/nemo-megasync-Debian_amd64.deb
 apt install -y /tmp/thunar-megasync-Debian_amd64.deb
-#XFCE
-#if [ $XDG_CURRENT_DESKTOP = XFCE ]; then fi
