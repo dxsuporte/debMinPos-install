08076381ea04dd586b6dfc15d09c02777674fb19
danilo <dxsuporteti@gmail.com>
2024-06-02 14:02:06 -0300

    up

--- appsdeb/conky.sh
+++ appsdeb/conky.sh
@@ -5,7 +5,7 @@ set -e
 apt install -y conky-all
 apt install -y fonts-font-awesome
 cp -f -r /etc/conky/conky.conf /etc/conky/bkp.conky.conf
-cp -f -r /config/conky/conky.conf /etc/conky/conky.conf
+cp -f -r config/conky/conky.conf /etc/conky/conky.conf
 #REDE
 ETH="$(lshw -c network | grep 'logical' | grep -m1 en | awk {'print $3'})"
 if [ $ETH ]; then
--- appsdeb/xampp.sh
+++ appsdeb/xampp.sh
@@ -3,15 +3,13 @@
 set -e
 #Verifica se o arquivo está dentro da pasta principal.
 if [ -e ./xampp-linux*.run ]; then
-    #Permissão de execução como programa.
     chmod +x xampp-linux*.run
-    #Executa o instalador Xampp.
-    ./xampp-linux*.run
+    sh xampp-linux*.run
 else
     RELEASE=$(curl -s https://sourceforge.net/projects/xampp/files/XAMPP%20Linux/ | grep -m1 "net.sf.files" | cut -d '"' -f 2)
-    wget -c https://sinalbr.dl.sourceforge.net/project/xampp/XAMPP%20Linux/"$RELEASE"/xampp-linux-x64-"$RELEASE"-0-installer.run
-    chmod +x xampp-linux*.run
-    ./xampp-linux*.run
+    wget -c https://sinalbr.dl.sourceforge.net/project/xampp/XAMPP%20Linux/"$RELEASE"/xampp-linux-x64-"$RELEASE"-0-installer.run -O /tmp/xampp-linux-x64-installer.run
+    chmod +x /tmp/xampp-linux-x64-installer.run
+    sh /tmp/xampp-linux-x64-installer.run
 fi
 #Copiá o script de configuração da permissão pkexec.
 cp -f config/xampp/org.apachefriends.xampp-control-panel.policy /usr/share/polkit-1/actions/
@@ -25,5 +23,3 @@ cp -f config/xampp/xampp.svg /usr/share/icons/
 cp -f config/xampp/xampp.service /etc/systemd/system/
 #Habilita o script no systemd para iniciar no boot.
 systemctl enable xampp.service
-#Mensagem para o usuário.
-echo "Foi instalado com sucesso"
