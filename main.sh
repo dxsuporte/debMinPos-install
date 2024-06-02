f62840baed5e182bf01fdfb32a9558391ce41af0
danilo <dxsuporteti@gmail.com>
2024-06-02 09:40:34 -0300

    up

--- main.sh
+++ main.sh
@@ -21,43 +21,42 @@ apt install -y firmware-amd-graphics amd64-microcode amdgcn-tools
 apt install -y intel-microcode intel-gpu-tools
 apt install -y mesa-vulkan-drivers mesa-utils libglapi-mesa libgl1-mesa-dri
 apt install -y task-laptop tlp
-#Interface grafica remove app do padrão
-apt update && apt remove libreoffice-* -y
-#Remove xfce
-apt remove -y xsane xterm exfalso hv3
-#Remove cinnamon
-apt remove -y hexchat pidgin thunderbird sound-juicer deja-dup gnote seahorse mate-icon-theme
-apt remove -y gnome-logs gnome-games gnome-font-viewer gnome-characters
+#Remove apps
+apt remove -y xsane xterm exfalso hv3 libreoffice-*
 apt autoremove -y
 #Mudar lingual do Sistema
-apt install -y locales-all
+apt install -y locales-all task-brazilian-portuguese*
 update-locale LANG=pt_BR.UTF-8 && locale-gen --purge pt_BR.UTF-8
-apt install -y task-brazilian-portuguese*
-#Interface grafica XFCE4
-#apt install -y xfce4-panel-profiles
-#Software extras XFCE
-apt install -y menulibre mugshot gigolo deepin-deb-installer gvfs*
+#Software extras
+apt install -y xfce4-panel-profiles menulibre mugshot gigolo deepin-deb-installer mate-calc drawing kazam
 #Gerenciador geral do sistema.
-apt install -y gnome-system-tools xdg-user-dirs-gtk elementary-xfce-icon-theme bibata-cursor-theme
-#Software Padrão
-apt install -y mate-calc drawing kazam
+apt install -y gvfs* gnome-system-tools xdg-user-dirs-gtk tracker elementary-xfce-icon-theme bibata-cursor-theme
 #Gerenciador Boot Plymouth
 apt install -y plymouth-themes grub-theme-starfield
 plymouth-set-default-theme -R bgrt
-#Programas de Shell e Config Net-tools
-apt install -y net-tools command-not-found neofetch arping
-echo 'PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/usr/bin/X11:/snap/bin"' | tee /etc/environment
 #Codecs para descompaquitar arquivos
 apt install -y p7zip* unrar* rar arc arj cabextract lhasa unace* sharutils uudeview mpack
 #Codecs de audio e video
 apt install -y ffmpeg mencoder faad lame sox twolame vorbis-tools
 #Outros Codecs
 apt install -y python3-unidecode
-#Gerenciador e atualizador de pacotes
-apt install -y package-update-indicator
-sed -i '3i Name[pt_BR]=Gerenciador de atualização de pacote' /usr/share/applications/org.guido-berhoerster.code.package-update-indicator.preferences.desktop
-sed -i '5i GenericName[pt_BR]=Preferências do indicador de atualização de pacote' /usr/share/applications/org.guido-berhoerster.code.package-update-indicator.preferences.desktop
-####Config####
+#Programas de Shell e Config Net-tools
+apt install -y net-tools command-not-found neofetch arping
+echo 'PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/usr/bin/X11:/snap/bin"' | tee /etc/environment
+#Copiar aquivo de configurações
+cp -f -r config/usr/* /usr/
+cp -f -r config/etc/* /etc/
+#Configuração do Skel
+mkdir -p /etc/skel/.config/
+cp -f -r config/etc/xdg/ /etc/skel/.config/
+#Remover Configurações
+rm -f -r /usr/share/icons/elementary-xfce-darke*
+rm -f -r /usr/share/themes/Daloa*
+rm -f -r /usr/share/themes/Kokodi*
+rm -f -r /usr/share/themes/Moheli*
+#Desktop Base
+unlink /etc/alternatives/desktop-theme
+ln -s /usr/share/desktop-base/dx-theme/ /etc/alternatives/desktop-theme
 #Login Lightdm
 sed -i 's/#greeter-hide-users=false/greeter-hide-users=false/g' /etc/lightdm/lightdm.conf
 #Grub
@@ -68,14 +67,6 @@ sed -i 's/#GRUB_GFXMODE=640x480/GRUB_GFXMODE=1024x768/g' /etc/default/grub
 sed -i 's/#GRUB_DISABLE_RECOVERY=true/GRUB_DISABLE_RECOVERY=true/g' /etc/default/grub
 sed -i 's/starfield.png/\/usr\/share\/images\/desktop-base\/desktop-grub.png/g' /usr/share/grub/themes/starfield/theme.txt
 grub-mkconfig -o /boot/grub/grub.cfg
-#Copy Files Configs
-cp -f -r config/usr/* /usr/
-cp -f -r config/etc/* /etc/
-mkdir -p /etc/skel/.config/
-cp -f -r config/etc/xdg/ /etc/skel/.config/
-#Desktop Base
-unlink /etc/alternatives/desktop-theme
-ln -s /usr/share/desktop-base/dx-theme/ /etc/alternatives/desktop-theme
 #Backgrounds
 ln -sf /usr/share/backgrounds/xfce/* /usr/share/backgrounds/
 ln -sf /usr/share/desktop-base/dx-theme/login/background.svg /usr/share/backgrounds/default.svg
@@ -94,11 +85,6 @@ ln -sf /usr/share/desktop-base/lines-theme/login/background-nologo.svg /usr/shar
 ln -sf /usr/share/desktop-base/moonlight-theme/login/background-nologo.svg /usr/share/backgrounds/13.svg
 ln -sf /usr/share/desktop-base/softwaves-theme/login/background-nologo.svg /usr/share/backgrounds/14.svg
 ln -sf /usr/share/plymouth/themes/emerald/Emerald_plymouth.svg /usr/share/backgrounds/15.svg
-#Remover themas não usados
-rm -f -r /usr/share/icons/elementary-xfce-darke*
-rm -f -r /usr/share/themes/Daloa*
-rm -f -r /usr/share/themes/Kokodi*
-rm -f -r /usr/share/themes/Moheli*
 #----------End----------#
 #Atualizar Grub, Limpeza apt
 update-grub2 && apt autoremove -y && apt autoclean && apt clean
@@ -109,3 +95,7 @@ if [ "$REBOOT" = "y" ]; then
 else
     rm -f -r /tmp/* && reboot
 fi
+
+#gsettings set org.gnome.desktop.interface color-scheme default
+#gsettings set org.gnome.desktop.interface color-scheme prefer-dark
+#runuser -l $(id 1000 -u -n) -c 'bash -c "$(wget https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh -O -)" --unattended' || echo 'OK'
