30638e8f28b57c25f6cb3c8a69fe3746cd7fb543
danilo <dxsuporteti@gmail.com>
2024-06-02 14:42:34 -0300

    up

--- appsdeb/ohmybash.sh
+++ appsdeb/ohmybash.sh
@@ -3,8 +3,8 @@
 set -e
 #----------Start----------#
 #Theme Bash User Root
-bash -c "$(wget https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh -O -)" --unattended || echo 'OK'
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)" --unattended || echo 'OK'
 sed -i 's/OSH_THEME="font"/OSH_THEME="zork"/g' /root/.bashrc
 #Theme Bash User First Home
-runuser -l $(id 1000 -u -n) -c 'bash -c "$(wget https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh -O -)" --unattended' || echo 'OK'
+runuser -l $(id 1000 -u -n) -c 'bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)" --unattended' || echo 'OK'
 sed -i 's/OSH_THEME="font"/OSH_THEME="mairan"/g' /home/$(id 1000 -u -n)/.bashrc
