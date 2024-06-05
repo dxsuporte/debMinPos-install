252b5e424eb92148787552abbb942ff319a85a07
danilo <dxsuporteti@gmail.com>
2024-06-04 21:26:02 -0300

    up

--- appsdeb/edge.sh
+++ appsdeb/edge.sh
@@ -8,8 +8,3 @@ echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft
 rm -f packages.microsoft.gpg
 apt update
 apt install -y microsoft-edge-stable
-
-#wget -qO - https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor -o /etc/apt/keyrings/microsoft-edge.gpg
-#echo "deb [signed-by=/etc/apt/keyrings/microsoft-edge.gpg] https://packages.microsoft.com/repos/edge stable main" | tee /etc/apt/sources.list.d/microsoft-edge.list
-#apt update
-#apt install -y microsoft-edge-stable
